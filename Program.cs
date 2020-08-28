f4ed897d207abc8ef6207bf5e4ee3728008f59b3
Max <maxttbmw@gmail.com>
2020-08-28 15:41:22 +0800

    Revert "Revert "Revert "delite"""
    This reverts commit 4db067f3bd64a5739d0e96c6a0c42866df32ff2b.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("Hello World!");
         }
     }
 }
